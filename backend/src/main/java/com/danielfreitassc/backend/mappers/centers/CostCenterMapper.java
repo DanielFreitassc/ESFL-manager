c3387ae51d7948f84a4d74a3cdcf5200c9b64c7d
Daniel Freitas <daniel.freitas@satc.edu.br>
2025-09-29 17:41:15 -0300

    Feat: update versão do spring

--- backend/src/main/java/com/danielfreitassc/backend/mappers/centers/CostCenterMapper.java
+++ backend/src/main/java/com/danielfreitassc/backend/mappers/centers/CostCenterMapper.java
@@ -15,8 +15,10 @@ public interface CostCenterMapper {
    @Mapping(target = "type", expression = "java(costCenterEntity.getType().getPtName())")
    CostCenterResponseDto toDto(CostCenterEntity costCenterEntity);
    @Mapping(target = "id", ignore = true)
+   @Mapping(target = "approved", ignore = true)
    CostCenterEntity toEntity(CostCenterRequestDto costCenterRequestDto);
    @Mapping(target = "id", ignore = true)
+   @Mapping(target = "approved", ignore = true)
    void toUpdate(CostCenterRequestDto centerRequestDto,@MappingTarget CostCenterEntity centerEntity);
 
 
